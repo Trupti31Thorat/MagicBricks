e84aaf7e8bb1425182a2af701e1981c66f5a28ad
Deokar <shreya-sanjay.deokar@capgemini.com>
2025-07-10 20:13:28 +0530

    Automation day-5

--- MagicBricksDemo/src/test/java/com/loginrunner/LoginRunner.java
+++ MagicBricksDemo/src/test/java/com/loginrunner/LoginRunner.java
@@ -3,18 +3,14 @@ package com.loginrunner;
 import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
-import io.cucumber.testng.AbstractTestNGCucumberTests;
+
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
     features = "src/test/resource/features",
     glue = {"com.loginstepdefinition"},
-    plugin = {"pretty",
-    		"html:target/cucumber-report.html",
-    		"json:target/cucumber.json",
-    		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
-    		},
+		plugin = { "pretty", "html:target/cucumber-report.html", "json:target/cucumber.json", },
     tags = "@Login"
 )
 
-public class LoginRunner extends AbstractTestNGCucumberTests{}
+public class LoginRunner {}
--- MagicBricksDemo/src/test/java/com/parameters/ConfigReader.java
+++ MagicBricksDemo/src/test/java/com/parameters/ConfigReader.java
@@ -6,25 +6,24 @@ import java.util.Properties;
 
 public class ConfigReader {
 
-    private static Properties prop;
+	private static Properties prop;
 
-    public static Properties getProperties() {
-        if (prop == null) {
-            prop = new Properties();
-            try {
-                FileInputStream fis = new FileInputStream(
-                    "C:\\Users\\SHRDEOKA\\eclipse-workspace\\MagicBricksDemo\\src\\test\\resource\\Property\\config.properties"
-                );
-                prop.load(fis);
-            } catch (IOException e) {
-                e.printStackTrace();
-                throw new RuntimeException("❌ Failed to load config.properties");
-            }
-        }
-        return prop;
-    }
+	public static Properties getProperties() {
+		if (prop == null) {
+			prop = new Properties();
+			try {
+				FileInputStream fis = new FileInputStream(
+						"C:\\Users\\SHRDEOKA\\eclipse-workspace\\MagicBricksDemo\\src\\test\\resource\\Property\\config.properties");
+				prop.load(fis);
+			} catch (IOException e) {
+				e.printStackTrace();
+				throw new RuntimeException("❌ Failed to load config.properties");
+			}
+		}
+		return prop;
+	}
 
-    public static String get(String key) {
-        return getProperties().getProperty(key);
-    }
+	public static String get(String key) {
+		return getProperties().getProperty(key);
+	}
 }
